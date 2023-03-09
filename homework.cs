f0aced9e52b24eaf2bec22edcff843c52fbd6834
zer0-cat <fedonovd@bk.ru>
2023-03-09 22:48:06 +0700

    Добавьте файлы проекта.

---
+++ homework.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork
+{
+    internal class homework
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Привет, Мир!");
+        }
+    }
+}
